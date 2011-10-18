63733232d8067032b3fd55d0e2cd0c8f3983a95b
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-10-17 21:59:50 -0400

    Add repository for Ubuntu Oneiric.

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "chris.peplin@rhubarbtech.com"
 license          "Apache 2.0"
 description      "Installs virtualbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.1"
+version          "0.6.2"
