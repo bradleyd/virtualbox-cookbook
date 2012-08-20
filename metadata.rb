7d64f89e8ca88839121749cecb013a27f36d4a26
Mike Adolphs <mike@fooforge.com>
2012-08-20 22:05:28 +0200

    Added dmg cookbook as a dependency

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,5 @@ license          "Apache 2.0"
 description      "Installs virtualbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.2"
+
+depends          "dmg"
