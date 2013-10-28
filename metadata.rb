d306b3a3263ec01d85b6f47a9a50d80a2284c265
Jason Walton <jwalton@benbria.ca>
2013-10-27 21:03:48 -0400

    Bump virtualbox cookbook version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chris.peplin@rhubarbtech.com"
 license          "Apache 2.0"
 description      "Installs virtualbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 %w{ubuntu debian centos redhat mac_os_x windows}.each do |os|
   supports os
