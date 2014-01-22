568c45929f6e2989dc882e48d354737688574253
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-01-21 19:02:09 -0500

    Fedora support
    * Add *.lock to .gitignore
    * Bump rhel/fedora version to 4.3
    * Add supports fedora to metadata
    * Refactor rhel,fedora yum_repository call
    * Add dpkg/rpm bats validation test

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs virtualbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.2"
 
-%w{ubuntu debian centos redhat mac_os_x windows}.each do |os|
+%w{ubuntu debian centos redhat mac_os_x windows fedora}.each do |os|
   supports os
 end
 
