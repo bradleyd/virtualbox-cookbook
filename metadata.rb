8a4bfb25a80a2230c6e6f04d76463c6e8d845302
Bradley Smith <brad.smith@sendgrid.com>
2014-03-22 14:05:22 -0600

    update yum dependency to version 3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chris.peplin@rhubarbtech.com"
 license          "Apache 2.0"
 description      "Installs virtualbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 %w{ubuntu debian centos redhat mac_os_x windows fedora}.each do |os|
   supports os
@@ -13,5 +13,5 @@ end
 depends "dmg"
 depends "windows"
 depends "apt"
-depends "yum"
+depends "yum", '~> 3.1'
 depends "apache2"
