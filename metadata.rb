d27459575f59f38182c931dbe69a7b61f4830905
Bradley Smith <brad.smith@sendgrid.com>
2014-03-22 14:05:22 -0600

    update yum dependency to version 3

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "virtualbox"
-maintainer       "Chris Peplin"
-maintainer_email "chris.peplin@rhubarbtech.com"
+maintainer       "Bradley D Smith"
+maintainer_email "bradleydsmith@gmail.com"
 license          "Apache 2.0"
 description      "Installs virtualbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
